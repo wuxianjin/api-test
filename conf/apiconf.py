0adc76ffcb97fc54bf7bc6161197d2c8f6bed0b9
wuxianjin <wuxianjin@users.noreply.github.com>
2018-09-05 19:36:29 +0800

    Update apiconf.py

--- conf/apiconf.py
+++ conf/apiconf.py
@@ -6,10 +6,10 @@
 @time: 2018-03-26-15 
 """
 # host
-API_HOST = 'bdp.cn'
+API_HOST = ''
 API_PORT = 443
 # host
-# API_HOST = '5xianjin.bdpdev.bdp.cn'
+# API_HOST = ''
 # API_PORT = 80
 # timeout
 TIME_OUT = 600
@@ -24,26 +24,12 @@ DOWNLOAD_DIR = "/tmp/"
 
 # user bdp_autotest
 
-USER_NAME = "admin"
-PASS_WORD = "123qweasd"
-BDP_DOMAIN = "ios"
 
-# for openbdp bdp_autotest
-# OPENBDP_HOST = '123.126.105.32'
-# OPENBDP_PORT = 15890
-# OPENBDP_TOKEN = '8e9548ad8e7645a3a40560828c488366'
-# OPENBDP_VERSION = 'v2'
-#
-OPENBDP_HOST = 'openapi.bdp.cn'
-OPENBDP_PORT = 443
-# admin opends_token
-OPENBDP_TOKEN = '6a6280e8489697d1a8d54c611815bd28'
-OPENBDP_VERSION = 'v2'
 
 # paralize
 PROCESS_COUNT = 20
 WAIT_TIME_OUT = 800
 
 # prepare project
-PROJECT_NAME = "BDP_BASIC_PROJECT"
+PROJECT_NAME = "_BASIC_PROJECT"
 PROJECT_NUM = 10
